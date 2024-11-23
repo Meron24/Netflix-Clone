79f7fd8de9c7e55cf7a3cda1e841630865fce592
Meron24 <meronmeazah@gmail.com>
2024-11-23 15:31:06 +0300

    header & footer

---
+++ netflix/src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../Componentes/Header/Header'
+import Footer from '../../Componentes/Footer/Footer'
+const Home = () => {
+  return (
+    <>
+        <Header />
+        <Footer />
+    </>
+  )
+}
+
+export default Home
