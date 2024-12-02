e1b4d14a31fae30d6d79d8101f70b5c486e7d322
Meron24 <meronmeazah@gmail.com>
2024-12-02 16:05:28 -0500

    Banner

--- netflix/src/Pages/Home/Home.js
+++ netflix/src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../Componentes/Header/Header'
 import Footer from '../../Componentes/Footer/Footer'
+import Banner from '../../Componentes/Banner/Banner'
 const Home = () => {
   return (
     <>
         <Header />
+        <Banner/>
         <Footer />
     </>
   )
