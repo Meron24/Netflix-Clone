fa050075fcd26cf450945ff62a7ef5c69143a84f
Meron24 <meronmeazah@gmail.com>
2024-12-02 23:15:51 -0500

    Rows

--- netflix/src/Pages/Home/Home.js
+++ netflix/src/Pages/Home/Home.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from '../../Componentes/Header/Header'
 import Footer from '../../Componentes/Footer/Footer'
 import Banner from '../../Componentes/Banner/Banner'
+import RowList from '../../Componentes/Rows/Row/RowList/RowList'
 const Home = () => {
   return (
     <>
         <Header />
         <Banner/>
+        <RowList/>
         <Footer />
     </>
   )
